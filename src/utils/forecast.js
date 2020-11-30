f2b996fb9d4dee5b6ee783e09cec8be3d862e27f
christianMlane011 <christianMlane011@gmail.com>
2020-11-30 14:47:57 -0500

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) { 
       callback('Unable to find weather for location', undefined);
     } else {
-      const data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.'
+      const data = body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The humidity is ' + body.current.humidity + '%.' 
       callback(undefined, data);
     }
   });
